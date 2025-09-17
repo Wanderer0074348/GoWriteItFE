fb806f014d314e0f9ecbf406274e1e0e0cf80db9
Tanay Matta <tanaymatta27@gmail.com>
2025-09-17 22:41:29 +0000

    revert

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,6 @@ import { PenSquare } from 'lucide-react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { GoogleIcon } from "@/components/icons/google";
-import { AnimatedBackground } from '@/components/animated-background';
 
 function ConversationIcon(props: React.SVGProps<SVGSVGElement>) {
     return (
@@ -37,9 +36,9 @@ function ConversationIcon(props: React.SVGProps<SVGSVGElement>) {
 export default function LoginPage() {
   return (
     <>
-      <AnimatedBackground />
-      <div className="container relative z-10 min-h-[calc(100vh-10rem)] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col p-10 text-white dark:border-r lg:flex">
+      <div className="container relative min-h-[calc(100vh-10rem)] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <PenSquare className="mr-2 h-6 w-6" />
             GoWriteIt
@@ -55,6 +54,7 @@ export default function LoginPage() {
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <ConversationIcon className="mx-auto mt-8 h-48 w-48 text-primary/20" />
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Let's connect.
